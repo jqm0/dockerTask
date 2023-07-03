d2a98aaef8989c4d402e74578509370e2dd30613
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-07-02 23:57:52 -0700

    Create a spring boot project for Docker

---
+++ DockerTask/src/main/java/com/example/demo/Controller/DisplayController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DisplayController {
+    @GetMapping
+    public String hello() {
+        return "Hello Tech Tribe";
+    }
+
+}
